89b6a62877d4eea11b620f17c07361a8fcfeec8c
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-18 15:38:29 +0100

    git branch -M main

---
+++ 📘 Chapitre 2 – Fonctions simples/script2.js
@@ -0,0 +1,16 @@
+ let x=20;
+ let y='3';
+ let Z=x+y;
+ console.log(Z);
+ console.log(+y)
+  x= x+4;
+y=y-1;
+console.log(x+y);
+Z=-Z*2-2*x;
+console.log(Z);
+Z=3*(y=z+20);
+console.log(Z);
+console.log(x%3);
+x=x%3**2;
+console.log(x);
+
