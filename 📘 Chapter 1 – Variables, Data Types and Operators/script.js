55981bedd806fcc12e7efdb7ecfbdc3683dab25d
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-18 15:37:33 +0100

    add 📘 Chapter 3 – Conditions and Boolean Logic

--- 📘 Chapter 1 – Variables, Data Types and Operators/script.js
+++ 📘 Chapter 1 – Variables, Data Types and Operators/script.js
@@ -1,5 +1,4 @@
-function addition(a, b,c) {
-    return a - b * c ;
-  }
-  let resultat = addition(4, 6,7);
-  console.log(resultat);
+let nombre1 = 10;
+let nombre2 = 5;
+let somme = nombre1 + nombre2;
+console.log("La somme est : " + somme);
