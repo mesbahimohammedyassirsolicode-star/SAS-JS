58d2e49a21720a7d664d2a7ee9cba2b66ae0d333
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-19 12:00:50 +0100

    Add Conditions-exo2 folder with index.html and script.js

---
+++ Conditions-exo2/script.js
@@ -0,0 +1,12 @@
+let note = prompt("entry your note");
+if (note < 10) {
+	alert("Insuffisant");
+} else {
+    if (note <=12){
+        alert("passable");
+
+    }else { 
+        alert("bien") ;
+    }
+}
+
