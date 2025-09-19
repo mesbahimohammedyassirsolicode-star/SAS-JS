b77eb752c94eb33a54c91e09dc3768019d867bb1
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-19 15:59:53 +0100

    add the files

---
+++ Conditions-exo3/script.js
@@ -0,0 +1,14 @@
+let continueInput = true;
+
+while (continueInput){
+   let nomber = prompt("d’entrer un nombre");
+if (nomber == 1) alert("lundi");
+else if (nomber ==2)alert("mardi")
+    else if (nomber == 3)alert("mercredi")
+else if (nomber==4)alert("jeudi")
+    else if (nomber==5)alert("vendredi")
+else if (nomber==6)alert("samedi")
+    else if (nomber==7) alert("dimanche")
+else alert("Numéro invalide") 
+}
+        continueInput = false;
---
+++ Conditions-exo4/script.js
@@ -0,0 +1,3 @@
+let nomber1 =prompt("saise un nomber");
+let nomber2 =prompt("saise un nomber");
+let op =prompt("saise un op");
