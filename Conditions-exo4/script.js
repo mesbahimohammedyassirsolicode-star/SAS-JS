befed6df573f1660de0123da90eea548586f3026
mesbahi mohammed yassir <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-21 21:30:38 +0100

    Update script.js

--- Conditions-exo4/script.js
+++ Conditions-exo4/script.js
@@ -1,3 +1,36 @@
-let nomber1 =prompt("saise un nomber");
-let nomber2 =prompt("saise un nomber");
-let op =prompt("saise un op");
+
+let number1 = prompt("Enter the first number:"));
+let number2 = prompt("Enter the second number:"));
+
+let operator = prompt("Enter an operator (+, -, *, /):");
+
+let result; // We declare a variable to store the result.
+
+switch (operator) {
+    case '+':
+        result = number1 + number2;
+        break; // 'break' stops the execution of the switch.
+
+    case '-':
+        result = number1 - number2;
+        break;
+
+    case '*':
+        result = number1 * number2;
+        break;
+
+    case '/':
+        
+        if (number2 === 0) {
+            result = "Division by zero is forbidden!";
+        } else {
+            result = number1 / number2;
+        }
+        break;
+
+    
+    default:
+        result = "Invalid operator. Please choose from +, -, *, /.";
+        break;
+}
+alert("Result: " + result);
