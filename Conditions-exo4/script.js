24e946bb5e1ccca965296bb6747c64a8426dd51f
mesbahi mohammed yassir <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-22 09:56:14 +0100

    Update script.js

--- Conditions-exo4/script.js
+++ Conditions-exo4/script.js
@@ -1,36 +1,43 @@
+let continueInput = true;
+while (continueInput) {
+    let number1 = prompt("Enter the first number:");
+    let number2 = prompt("Enter the second number:");
 
-let number1 = prompt("Enter the first number:");
-let number2 = prompt("Enter the second number:");
+    let operator = prompt("Enter an operator (+, -, *, /):");
 
-let operator = prompt("Enter an operator (+, -, *, /):");
+    let result; 
+    if (number1 === null || number2 === null || operator === null) {
+        alert("Operation cancelled.");
+        result = "No result due to cancellation.";
+        continueInput = false;
+    } else {
+        number1 = Number(number1);
+        number2 = Number(number2);
 
-let result; 
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                break; 
+            case '-':
+                result = number1 - number2;
+                break;
 
-switch (operator) {
-    case '+':
-        result = number1 + number2;
-        break; // 
+            case '*':
+                result = number1 * number2;
+                break;
 
-    case '-':
-        result = number1 - number2;
-        break;
+            case '/':
+                if (number2 === 0) {
+                    result = "Division by zero is forbidden!";
+                } else {
+                    result = number1 / number2;
+                }
+                break;
 
-    case '*':
-        result = number1 * number2;
-        break;
-
-    case '/':
-        
-        if (number2 === 0) {
-            result = "Division by zero is forbidden!";
-        } else {
-            result = number1 / number2;
+            default:
+                result = "Invalid operator. Please choose from +, -, *, /.";
+                break;
         }
-        break;
-
-    
-    default:
-        result = "Invalid operator. Please choose from +, -, *, /.";
-        break;
+    }
+    alert("Result: " + result);
 }
-alert("Result: " + result);
