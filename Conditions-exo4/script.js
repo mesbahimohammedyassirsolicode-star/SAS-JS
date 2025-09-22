9f3c66548e4fe1fd9b0171a91d5cfb8c352253d0
mesbahi mohammed yassir <mesbahi.mohammedyassir.solicode@gmail.com>
2025-09-22 09:51:52 +0100

    Update script.js

--- Conditions-exo4/script.js
+++ Conditions-exo4/script.js
@@ -1,15 +1,15 @@
 
-let number1 = prompt("Enter the first number:"));
-let number2 = prompt("Enter the second number:"));
+let number1 = prompt("Enter the first number:");
+let number2 = prompt("Enter the second number:");
 
 let operator = prompt("Enter an operator (+, -, *, /):");
 
-let result; // We declare a variable to store the result.
+let result; 
 
 switch (operator) {
     case '+':
         result = number1 + number2;
-        break; // 'break' stops the execution of the switch.
+        break; // 
 
     case '-':
         result = number1 - number2;
