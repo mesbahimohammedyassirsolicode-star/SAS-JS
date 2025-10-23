e6b85943c0d098b83b11c996bda9ff76f999b795
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-10-23 10:28:51 +0100

    add

---
+++ 📘 Chapter 5 – Tables in JavaScript (without DOM)/script.js
@@ -0,0 +1,46 @@
+//1.creation a table of books
+let books = [
+        {title:"ice song and fire",author:"gorge martin",price:100 },
+        {title:"the prince",author:"Niccolo Machiavelli" , price:99},
+        {title:"beyond good and devil ",author:"Friedrich Nietzsche",price:200},
+        {title:"The Hobbit", author: "J.R.R. Tolkien", price: 150 },
+        {title:"The Lord of the Rings", author: "J.R.R. Tolkien", price: 150 },
+
+] 
+function bookslist(books){
+  console.log("---The books list---");
+  for (let i=0;i< books.length ; i++ )
+    console.log(books[i]);
+    
+  
+}
+function showTitlesOnly(books) {
+ console.log("----BOOK TITLES ONLY----");
+ for (let i =0 ;i< books.length;i++)
+  console.log(books[i].title);
+} 
+
+
+function count() {
+  console.log("---total price---"); 
+  let count=0; 
+for (let i=0;i< books.length;i++){
+count+=books[i].price;
+}
+console.log(count);
+}
+
+function overprice(books) {
+  console.log("---the books over 100");
+  for (let i=0;i<books.length;i++){
+ if (books[i].price>=100){
+    console.log(books[i].title);
+}
+  }
+ 
+  }
+books.push({title:"fire and blood",author:"gorge martin",price:149})
+bookslist(books);
+showTitlesOnly(books);
+count();
+overprice(books);
