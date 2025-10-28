8d8a05ccd2461219a8a982dc07cac5487c84870b
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-10-28 13:48:57 +0100

    updt

--- Prototype script/script.js
+++ Prototype script/script.js
@@ -1,5 +1,5 @@
 let bibliotheque = [
-    {code: 1, title: "ice song and fire", author: "gorge martin", year: 1996, disponible: true, price: "100dh", image: "images/ice-song-and-fire.jpg"},
+    {code: 1, title: "A Song of Ice and Fire", author: "gorge martin", year: 1996, disponible: true, price: "100dh", image: "images/ice-song-and-fire.jpg"},
     {code: 2, title: "the prince", author: "Niccolo Machiavelli", year: 1532, disponible: true, price: "99dh", image: "images/the-prince.jpg"},
     {code: 3, title: "beyond good and devil", author: "Friedrich Nietzsche", year: 1886, disponible: false, price: "200dh", image: "images/beyond-good-and-evil.jpg"},
     {code: 4, title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937, disponible: true, price: "150dh", image: "images/the-hobbit.jpg"},
@@ -11,11 +11,9 @@ let bibliotheque = [
 ];
 
 let databooks = document.querySelector(".databook");
-
 bibliotheque.forEach(book => {
     let card = document.createElement("div");
     card.classList.add("card");
-    
     card.innerHTML = ` 
         <div class="book-img" style="background-image: url('${book.image}')"></div>
         <h2>${book.title}</h2>
@@ -25,12 +23,10 @@ bibliotheque.forEach(book => {
         <p><strong>Prix:</strong> ${book.price}</p>
         <p><strong>code:</strong>${book.code}</p>
         <button class="btn-supprimer">Supprimer</button>`;
-
     let btnsupprimer = card.querySelector(".btn-supprimer");
     btnsupprimer.addEventListener("click", function() {
         card.remove();
     });
-
     databooks.appendChild(card);
 });
 function totalbooks() {
@@ -38,7 +34,7 @@ function totalbooks() {
     for (let i = 0; i < bibliotheque.length; i++) {
         total = total + 1;
     }
-    
+
     return total;
     }
     function nombreLivresDisponibles() {
@@ -49,7 +45,35 @@ function totalbooks() {
             disponibles = disponibles + 1;
         }
     }
-    
     return disponibles;
+
+
+}
+//total books
+let total=document.querySelector(`.stat-number`);
+total.innerHTML=bibliotheque.length; 
+
+let disponible=document.getElementById("availableBooks");
+disponible.innerHTML=nombreLivresDisponibles();
+//serching bar
+let searchbar = document.getElementById('searchInput');
+
+function searchBooks() {
+    let value = searchbar.value.trim().toLowerCase();
+    let allCards = document.querySelectorAll(".card");
+    
+    for (let i = 0; i < bibliotheque.length; i++) {
+        let title = bibliotheque[i].title.trim().toLowerCase();
+        let card = allCards[i];
+        
+        if (value === '' || title.includes(value)) {
+            card.style.display = 'block';
+            card.classList.add("founded");
+        } else {
+            card.style.display = 'none';
+            card.classList.remove("founded");
+        }
+    }
 }
 
+searchbar.addEventListener("change", searchBooks);
